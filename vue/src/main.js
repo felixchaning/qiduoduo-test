1c7c7eeba00d0ed5ab3e0bbe4aca8f739c4337c8
chenli <felixchaning@gmail.com>
2022-07-27 20:19:51 +0800

    添加：登录表单

--- vue/src/main.js
+++ vue/src/main.js
@@ -1,10 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import Antd from 'ant-design-vue'
+import 'ant-design-vue/dist/antd.css'
 import './assets/main.css'
 
-const app = createApp(App)
+const app = createApp(App).use(Antd)
 
 app.use(router)
 
--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -1,9 +1,68 @@
-<script setup>
-import TheWelcome from '@/components/TheWelcome.vue'
+<template>
+  <a-form
+    :model="formState"
+    name="basic"
+    :label-col="{ span: 8 }"
+    :wrapper-col="{ span: 16 }"
+    autocomplete="off"
+    @finish="onFinish"
+    @finishFailed="onFinishFailed"
+    class="form"
+  >
+    <a-form-item
+      label="用户名"
+      name="username"
+      :rules="[{ required: true, message: '请输入用户名!' }]"
+    >
+      <a-input v-model:value="formState.username" />
+    </a-form-item>
+
+    <a-form-item
+      label="密码"
+      name="password"
+      :rules="[{ required: true, message: '请输入密码!' }]"
+    >
+      <a-input-password v-model:value="formState.password" />
+    </a-form-item>
+
+    <a-form-item name="remember" :wrapper-col="{ offset: 8, span: 16 }">
+      <a-checkbox v-model:checked="formState.remember">Remember me</a-checkbox>
+    </a-form-item>
+
+    <a-form-item :wrapper-col="{ offset: 8, span: 16 }">
+      <a-button type="primary" html-type="submit">登录</a-button>
+    </a-form-item>
+  </a-form>
+</template>
+<script>
+import { defineComponent, reactive } from 'vue'
+export default defineComponent({
+  setup() {
+    const formState = reactive({
+      username: '',
+      password: '',
+      remember: true,
+    })
+
+    const onFinish = (values) => {
+      console.log('Success:', values)
+    }
+
+    const onFinishFailed = (errorInfo) => {
+      console.log('Failed:', errorInfo)
+    }
+
+    return {
+      formState,
+      onFinish,
+      onFinishFailed,
+    }
+  },
+})
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<style scoped>
+.form {
+  width: 30vw;
+}
+</style>
