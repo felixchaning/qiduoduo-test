a6d3e8df213b85670ea98f06c9e99c57cc9f6817
chenli <felixchaning@gmail.com>
2022-07-27 18:39:18 +0800

    初始化项目

---
+++ midway/src/config/config.default.ts
@@ -0,0 +1,9 @@
+import { MidwayConfig } from '@midwayjs/core';
+
+export default {
+  // use for cookie sign key, should change to your own and keep security
+  keys: '1658918114993_710',
+  koa: {
+    port: 7001,
+  },
+} as MidwayConfig;
